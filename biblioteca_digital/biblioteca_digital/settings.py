7ac84e76bcb52b2b5abbac3bb6095df0f40f2524
melany <melanydiazmarquez@gmail.com>
2026-04-13 23:44:55 -0300

    Completar flujo de password reset y templates

--- biblioteca_digital/biblioteca_digital/settings.py
+++ biblioteca_digital/biblioteca_digital/settings.py
@@ -123,11 +123,14 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/libros/'
 LOGOUT_REDIRECT_URL = '/login/'
 
-# Configuración de email
+# Configuración de email para recuperacion de contraseña
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bibliois210@gmail.com'
-EMAIL_HOST_PASSWORD = 'mgta vloi jlnw skdo'
-DEFAULT_FROM_EMAIL = 'Biblioteca ISFD 210 <bibliois210@gmail.com>'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = 'Biblioteca ISFD 210 <tu-correo@gmail.com>'
+
+
